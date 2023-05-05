febf2fb9bff8040ca1a409e03643d03888ba5b09
NordStj <Neoskyping@gmail.com>
2023-05-05 09:42:34 +0300

    release version

--- SetColorView/ViewController.swift
+++ SetColorView/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewColor.layer.cornerRadius = 10
     }
 
     @IBAction func redSliderAction() {
