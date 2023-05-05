92798f8b8e5237c15718342292041c7e199fdff9
NordStj <Neoskyping@gmail.com>
2023-05-05 08:43:47 +0300

    Initial Commit

---
+++ SetColorView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SetColorView
+//
+//  Created by Дмитрий on 05.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
